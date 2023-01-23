1342b99369f7ee5aa4bbc4534c766c36b8237205
Stacyde <degtich@mail.ru>
2023-01-23 23:51:47 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 2_187;
+        int rub = 20;
+
+
+        int miles = ticketPrice/rub;
+
+        System.out.println(miles);
+    }
+}
