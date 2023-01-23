36cc2fff2f7f46156bd4b371137debe7d5718a83
Stacy <79156969+Stacyde@users.noreply.github.com>
2023-01-24 00:23:12 +0400

    refactoring

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,8 @@ public class Main {
     public static void main(String[] args) {
         int ticketPrice = 2_187;
         int rub = 20;
-
-
+        
         int miles = ticketPrice/rub;
-
         System.out.println("Количество начисленных миль за купленный билет = " + miles);
     }
 }
